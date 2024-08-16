4d6adbcb11d9b9f0a898d57f1ee4c0e09b25fecf
DHipo <bautidhipolito@gmail.com>
2024-08-16 11:10:29 -0300

    final implementation

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -1,14 +1,43 @@
 package com.template;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class App {
 
 
     public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+
+        if (!accounts.containsKey(account)) return accounts;
+        if (amount <= 0) return accounts;
+
+        Map<String, Integer> updatedAccounts = new HashMap<>(accounts);
+        updatedAccounts.put(account, accounts.get(account) + amount);
+        return updatedAccounts;
     }
 
+    public static Map<String, Integer> withdraw(Map<String, Integer> accounts, String account, int amount) {
 
+        if (!accounts.containsKey(account)) return accounts;
+        if (amount <= 0) return accounts;
+        if (accounts.get(account) - amount < 0) return accounts; // Saldo insuficiente
+
+        Map<String, Integer> updatedAccounts = new HashMap<>(accounts);
+        updatedAccounts.put(account, accounts.get(account) - amount);
+        return updatedAccounts;
+    }
+
+    public static Map<String, Integer> transfer(Map<String, Integer> accounts, String from_account, String to_account, int amount) {
+
+        if (Objects.equals(from_account, to_account)) return accounts;
+        if (!accounts.containsKey(to_account) || !accounts.containsKey(from_account)) return accounts;
+        if (accounts.get(from_account) - amount < 0) return accounts;
+        if (amount <= 0) return accounts;
+
+        Map<String, Integer> updatedAccounts = new HashMap<>(accounts);
+        updatedAccounts.put(from_account, accounts.get(from_account) - amount);
+        updatedAccounts.put(to_account, accounts.get(to_account) + amount);
+        return updatedAccounts;
+    }
 }
--- src/test/java/com/template/AppTest.java
+++ src/test/java/com/template/AppTest.java
@@ -4,15 +4,149 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static com.template.App.deposit;
+import static com.template.App.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
+
+    /*
+     *   ----------------- Deposit -----------------
+     */
 
     @Test
     void testDeposit() {
         Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
         Map<String, Integer> updatedAccounts = deposit(accounts, "mati", 5);
-        assertEquals(true, true);
+        Map<String, Integer> expectedResult = Map.of("mati", 15, "jaun", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testDepositWithNegativeAmount() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "mati", -5);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "jaun", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testDepositWithNoAccount() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "javi", 5);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "jaun", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    /*
+    *   ----------------- WITHDRAW -----------------
+     */
+
+    @Test
+    void testWithdraw() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "mati", 5);
+        Map<String, Integer> expectedResult = Map.of("mati", 5, "jaun", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testWithdrawWithNegativeAmount() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "mati", -5);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "jaun", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testWithdrawWithNoAccount() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "javi", 5);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "jaun", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testWithdrawWithInsufficientFunds() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "jaun", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "javi", 15);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "jaun", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    /*
+     *   ----------------- TRANSFER -----------------
+     */
+
+    @Test
+    void testTransfer() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "juan",5);
+        Map<String, Integer> expectedResult = Map.of("mati", 5, "juan", 25);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testTransferWhitNoAccount() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "topo", "juan",5);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "juan", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testTransferWithInsufficientFunds() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "juan",50);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "juan", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testTransferWithSameAccount() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "juan", 20);
+        Map<String, Integer> updatedAccounts = transfer(accounts, "mati", "mati",5);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "juan", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    /*
+     *   ----------------- OTHER TESTS -----------------
+     */
+
+    @Test
+    void testDepositAndTransfer() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "juan", 20);
+        Map<String, Integer> updatedAccounts = deposit(accounts, "mati", 5);
+        updatedAccounts = transfer(updatedAccounts, "mati", "juan",5);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "juan", 25);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testWithdrawAndTransfer() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "juan", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "mati", 5);
+        updatedAccounts = transfer(updatedAccounts, "mati", "juan",5);
+        Map<String, Integer> expectedResult = Map.of("mati", 0, "juan", 25);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testDepositAndWithdraw() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "juan", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "mati", 5);
+        updatedAccounts = deposit(updatedAccounts, "mati",5);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "juan", 20);
+        assertEquals(expectedResult, updatedAccounts);
+    }
+
+    @Test
+    void testAll() {
+        Map<String, Integer> accounts = Map.of("mati", 10, "juan", 20);
+        Map<String, Integer> updatedAccounts = withdraw(accounts, "mati", 5);
+        updatedAccounts = deposit(updatedAccounts, "mati",5);
+        updatedAccounts = transfer(updatedAccounts, "mati","juan", 15);
+        Map<String, Integer> expectedResult = Map.of("mati", 10, "juan", 20);
+        assertEquals(expectedResult, updatedAccounts);
     }
 }
